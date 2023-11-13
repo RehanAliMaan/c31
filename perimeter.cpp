1867b3f91a93be6dcb5d1233681ddce3a032064e
Rehan Ali maan <130174371+RehanAliMaan@users.noreply.github.com>
2023-11-13 12:10:53 +0500

    Add files via upload

---
+++ perimeter.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float l,w;
+	cout<<"enter the length:";
+	cin>>l;
+	cout<<"enter the width:";
+	cin>>w;
+	float p;
+	p=(l+w)*2;
+	cout<<"perimeter is:"<<p<<endl;
+	return 0;
+}
